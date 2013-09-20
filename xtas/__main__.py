dc1f3c0f4020936285d2c2fe89e281e42b833aed
Lars Buitinck <l.buitinck@esciencecenter.nl>
2013-09-20 15:57:07 +0200

    xtas (server|worker) command

--- xtas/__main__.py
+++ xtas/__main__.py
@@ -1,9 +1,14 @@
+from __future__ import print_function
+
+from argparse import ArgumentParser
+import sys
+
+from celery import Celery
+
 from .server import Server
 
 
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-
+def server_main():
     argp = ArgumentParser(description='xtas-lite server')
     argp.add_argument('--debug', action='store_true')
 
@@ -11,3 +16,35 @@ if __name__ == '__main__':
 
     server = Server(debug=args.debug)
     server.run()
+
+
+def worker_main():
+    argp = ArgumentParser(description='xtas-lite worker')
+    argp.add_argument('--debug', action='store_true')
+
+    args = argp.parse_args()
+
+    from .taskregistry import TASKS
+    from . import tasks
+
+    # TODO add options for these
+    celery = Celery(broker='amqp://guest@localhost//', backend='amqp')
+
+    for f, url in TASKS:
+        if args.debug:
+            print("Providing task %s" % url)
+        celery.task(f, name=url)
+
+    celery.worker_main()
+
+
+progs = {'server': server_main,
+         'worker': worker_main}
+
+try:
+    main = progs[sys.argv.pop(1)]
+    main()
+except IndexError, KeyError:
+    # XXX use argparse to handle this as well? docopt.org can do it...
+    print('usage: python -m xtas (server|worker) [options]', file=sys.stderr)
+    sys.exit(1)
