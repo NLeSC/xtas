2f4e167b048aaf4e09182ca60275f5162674a0fc
Lars Buitinck <l.buitinck@esciencecenter.nl>
2013-09-30 15:36:42 +0200

    fix tasks module
    Should have done this in 408ca5145b1548ddcd5b34c8f60188acbc99bacc.

--- xtas/tasks/__init__.py
+++ xtas/tasks/__init__.py
@@ -1,5 +1,5 @@
 # Make sure to import all the tasks here, so that they get registered with
 # the server class (see ../server.py).
 
-from .fail import fail
+from .test import test_task
 #from .vectorize import vectorize
