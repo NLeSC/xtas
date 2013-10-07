ec565290bf26c6f0c8280f9f320fb3dc596dca62
Lars Buitinck <l.buitinck@esciencecenter.nl>
2013-10-07 14:58:42 +0200

    installation instructions + celery requirement

--- README.rst
+++ README.rst
@@ -3,6 +3,20 @@ xtaslite
 
 Distributed text analysis suite based on Celery.
 
+
+Installation
+------------
+
+Set up a virtualenv and activate it, if desired. Then::
+
+    pip install git+https://github.com/NLeSC/xtas.git
+
+Next, make sure you have RabbitMQ running (currently only supported on localhost).
+
+
+Usage
+-----
+
 The xtas engine consists of two parts: a server that manages a task queue and
 offers a ReST API, and a set of workers. To start a server, do::
 
--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@
 
 from distutils.core import setup
 
-setup(name="xtas",
-      description="Distributed text analysis suite",
-      author="Lars Buitinck",
-      author_email="l.buitinck@esciencecenter.nl",
-      packages=["xtas", "xtas.tasks"],
-      classifiers=[
+setup(
+    name="xtas",
+    description="Distributed text analysis suite",
+    author="Lars Buitinck",
+    author_email="l.buitinck@esciencecenter.nl",
+    packages=["xtas", "xtas.tasks"],
+    classifiers=[
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Text Processing",
-      ],
-      install_requires=["flask>=0.10.1"],
+    ],
+    install_requires=[
+        "celery>=3.0.0",
+        "flask>=0.10.1",
+    ],
 )
