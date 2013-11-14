678a6e16b57e523f75c2f21158d3743486c590fc
Lars Buitinck <l.buitinck@esciencecenter.nl>
2013-11-14 15:58:59 +0100

    fix test task and make it sleep(5)

--- xtas/tasks/test.py
+++ xtas/tasks/test.py
@@ -1,8 +1,11 @@
 """Simple tasks for testing and debugging."""
 
+from time import sleep
+
 from ..taskregistry import task
 
 
-@task('/test')
+@task(takes=None)
 def trivial_task(config):
+    sleep(5)
     return "success"
