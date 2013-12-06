f91de21a89979c808f01ded2557ad0b2139baaaa
Lars Buitinck <l.buitinck@esciencecenter.nl>
2013-12-06 16:34:57 +0100

    add an example script for pipelines
    Mostly for my own reference; to be integrated into the main code.

---
+++ examples/pipeline.py
@@ -0,0 +1,13 @@
+# Simple example of how to construct a pipeline of operations from Python
+# and apply them to an ES document, storing the result back into ES.
+
+from celery import chain
+from xtas.tasks import *
+
+ch = chain(fetch_es.s('blog', 'post', 1, 'body')
+           | tokenize.s() | pos_tag.s('nltk')
+           | store_es.s('pipeline', 'blog', 'post', 1)
+          )
+
+r = ch.delay()
+print(r.get())
