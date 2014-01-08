e613cd589cf0021e2f4f9ebd50df18db7dd1f809
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-01-08 16:52:37 +0100

    useful debugging setting in celeryconfig (ALWAYS_EAGER)

--- celeryconfig.py
+++ celeryconfig.py
@@ -6,3 +6,6 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT=['json']
 CELERY_TIMEZONE = 'Europe/Amsterdam'
 CELERY_ENABLE_UTC = True
+
+# Uncomment the following to make Celery tasks run locally (for debugging).
+#CELERY_ALWAYS_EAGER = True
