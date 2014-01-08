9c5b6e7bdbefe47f26258e016729aaab62611c5f
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-01-08 16:07:15 +0100

    add field to run_task_on_es

--- rest.py
+++ rest.py
@@ -1,23 +1,28 @@
 from __future__ import absolute_import
 
+from celery import Task, chain
+import celery.result
 from flask import Flask, abort
 import json
+
 from xtas.tasks import app as taskq
 from xtas.tasks import fetch_es, store_es
 
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route("/run_es/<task>/<index>/<type>/<id>")
-def run_task_on_es(task, index, type, id):
+@app.route("/run_es/<task>/<index>/<type>/<id>/<field>")
+def run_task_on_es(task, index, type, id, field):
     try:
-        task = taskq.tasks['xtas.tasks.%s' % task]
+        taskname = 'xtas.tasks.%s' % task
+        task = taskq.tasks[taskname]
     except KeyError:
         abort(404)
 
-    return chain(fetch_es.s(index, type, id)
-                 | task
-                 | store_es(task, index, type, id)
+    return chain(fetch_es.s(index, type, id, field)
+                 | task.s()
+                 | store_es.s(taskname, index, type, id)
                 ).delay().id
 
 
