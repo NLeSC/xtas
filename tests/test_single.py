bb1a86dfa4cbfaa9cb91cd7a4d8f9a89e1296d31
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-02-10 12:09:11 +0100

    test for Stanford NER (skipped)

--- tests/test_single.py
+++ tests/test_single.py
@@ -1,6 +1,7 @@
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_true
+from unittest import skip
 
-from xtas.tasks import tokenize
+from xtas.tasks import stanford_ner_tag, tokenize
 
 
 def test_tokenize():
@@ -8,3 +9,14 @@ def test_tokenize():
     expected = "My hovercraft is full of eels .".split()
     for obs, exp in zip(tokens, expected):
         assert_equal(obs, {"token": exp})
+
+
+@skip
+def test_stanford_ner():
+    # From Wikipedia front page, 10 Feb 2014.
+    ne = stanford_ner_tag("Academy Award-winning actor Philip Seymour Hoffman"
+                          " dies at the age of 46.")
+    [sentence] = ne
+    for token, tag in sentence:
+        assert_true(isinstance(token, basestring))
+        assert_true(tag in ["O", "PERSON"])
