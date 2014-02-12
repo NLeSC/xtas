a4234a6a99b8e74fe028b8c75c00596eb1e32e34
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-02-12 15:23:43 +0100

    setup.py: remove package data (not used), extras_require
    No entry points for extras_require yet, mostly serves as documentation.

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ setup(
     author="Lars Buitinck",
     author_email="l.buitinck@esciencecenter.nl",
     packages=["xtas", "xtas.webserver"],
-    package_data={"xtas": ["templates/*.html"],
-                  "xtas.configure": ["*.yaml"]},
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
@@ -21,10 +19,12 @@ setup(
         "celery>=3.0.0",
         "flask>=0.10.1",
         #"langid>=1.1.4dev",
-        #"librabbitmq",
         "nltk",
         "rawes",
-        "scikit-learn>=0.13",
         #"simplejson>=2.6.2",
     ],
+    extras_requires={
+        "clustering": "scikit-learn>=0.13",
+        "fast": "librabbitmq",
+    },
 )
