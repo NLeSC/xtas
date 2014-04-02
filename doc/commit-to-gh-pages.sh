e51867de39b4e462ef11ffd58a6a8493b3c9624e
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-04-02 16:19:25 +0200

    cosmetic fixes to gh-pages stuff

--- doc/commit-to-gh-pages.sh
+++ doc/commit-to-gh-pages.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-# Hash the generated HTML docs and commit them to the branch gh-pages.
+# Commit the generated HTML pages to the branch gh-pages.
 # Will not push them to GitHub.
 
 set -e -v
 
-treehash=$(${PYTHON:-python} hash-tree.py "${1:-_build/html}")
+treehash=$(./hash-tree.py "${1:-_build/html}")
 parent=$(git rev-parse gh-pages)
 
 msg="Regenerated docs for $(git rev-parse HEAD)"
