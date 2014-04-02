c25aee8482fac6aee6055ab6306105382c749e9e
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-04-02 16:47:38 +0200

    bump version number: 2.99.4

--- release.sh
+++ release.sh
@@ -1,9 +1,17 @@
-set -e
+# Do a release. This is more a canned set of instructions than a script.
+
+set -e -v
+
 ${VISUAL} xtas/_version.py
 ${VISUAL} doc/conf.py
+git commit
+
 git tag -a v$(python -c 'execfile("xtas/_version.py"); print(__version__)')
 git push --tags
+
 clonedir=$(mktemp -d)
 git clone . "$clonedir"
 cd "$clonedir"
 python setup.py sdist upload
+
+echo "To push new docs: (cd doc && make gh-pages && git push)"
