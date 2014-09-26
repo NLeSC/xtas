c95762b4919000f94983bf2133516b740a2e334e
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-09-26 17:23:19 +0200

    shut nltk.download up

--- xtas/_utils.py
+++ xtas/_utils.py
@@ -3,10 +3,18 @@ from collections import Sequence
 import nltk
 
 
+_NLTK_DOWNLOADED = set()
+
+
 def nltk_download(package):
     # XXX we could set the download_dir to download to xtas_data/nltk_data
     # and have everything in one place.
-    nltk.download(package, raise_on_error=True, quiet=False)
+    if package not in _NLTK_DOWNLOADED:
+        # Ensure we don't get a log message for *every single* successful
+        # call, but we still log the actual downloads.
+        # Should fix this upstream by allowing quiet='success' or something.
+        nltk.download(package, raise_on_error=True, quiet=False)
+        _NLTK_DOWNLOADED.add(package)
 
 
 def tosequence(it):
