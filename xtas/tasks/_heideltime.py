1dcf395fecf0735c3486455db1d030d8dbc46220
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-12-16 13:45:46 +0100

    bugs bugs bugs

--- xtas/tasks/_heideltime.py
+++ xtas/tasks/_heideltime.py
@@ -8,7 +8,7 @@ from .._downloader import _download_zip, _make_data_home
 
 _heideltime_dir = _download_zip(url='http://goo.gl/mx5ckK', name='Heideltime',
                                 check_dir='heideltime-standalone')
-_jar = join(heideltime_dir, 'de.unihd.dbs.heideltime.standalone.jar')
+_jar = join(_heideltime_dir, 'de.unihd.dbs.heideltime.standalone.jar')
 
 _config_props = join(_heideltime_dir, 'config.props.xtas')
 
@@ -16,13 +16,15 @@ shutil.copyfile(join(_heideltime_dir, 'config.props'), _config_props)
 
 # Ugliest hack evah
 subprocess.call(['sed', '-i',
-                 's/^treeTaggerHome .*/treeTaggerHome = %s'
-                    % (join(_make_data_home()), 'treetagger'),
+                 's|^treeTaggerHome .*|treeTaggerHome = %s|'
+                    % (join(_make_data_home(), 'treetagger')),
                  _config_props])
 
 
 def call_heideltime(doc, language):
     with tempfile.NamedTemporaryFile(prefix='xtas-heideltime') as f:
+        f.write(doc)
+        f.flush()
         out = subprocess.check_output(['java', '-jar', _jar,
                                        '-c', _config_props,
                                        '-l', language, '-pos', 'treetagger',
