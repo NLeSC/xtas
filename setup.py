175fc7e76b1baa9cc3248eeae8d434816d07e111
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-10-14 17:14:05 +0200

    add *.xml to package_data

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email="l.buitinck@esciencecenter.nl",
     packages=["xtas", "xtas.make_config", "xtas.tasks", "xtas.tests",
               "xtas.webserver"],
-    package_data={"xtas.tasks": ["*.txt", "NERServer.class"]},
+    package_data={"xtas.tasks": ["*.txt", "*.xml", "NERServer.class"]},
     url="https://github.com/NLeSC/xtas",
     version=__version__,
     classifiers=[
