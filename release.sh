8664d1eb87cebced8bdb25c6c487818473cee060
Lars Buitinck <l.buitinck@esciencecenter.nl>
2016-02-15 17:27:18 +0100

    version 3.4
    * Emotion tagger
    * Documentation updates
    * Dependency updates

--- release.sh
+++ release.sh
@@ -4,7 +4,8 @@ set -e -v
 
 ${VISUAL} xtas/_version.py
 ${VISUAL} doc/conf.py
-git add xtas/_version.py doc/conf.py
+${VISUAL} doc/changelog.rst
+git add xtas/_version.py doc/conf.py doc/changelog.rst
 git commit
 
 git tag -a v$(python -c 'execfile("xtas/_version.py"); print(__version__)')
