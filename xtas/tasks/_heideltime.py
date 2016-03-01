aac65128cf6d10e662b61796cbb7505480f80dca
Lars Buitinck <l.buitinck@esciencecenter.nl>
2016-03-01 13:30:07 +0100

    upgrade Heideltime dep to 2.1, clean up code

--- xtas/tasks/_heideltime.py
+++ xtas/tasks/_heideltime.py
@@ -1,5 +1,4 @@
-from os.path import dirname, join
-import shutil
+from os.path import join
 import subprocess
 import tempfile
 import xml.etree.ElementTree as etree
@@ -7,19 +6,23 @@ import xml.etree.ElementTree as etree
 from .._downloader import _download_zip, _make_data_home
 
 
-_heideltime_dir = _download_zip(url='http://goo.gl/mx5ckK', name='Heideltime',
+_zip = 'https://github.com/HeidelTime/heideltime/releases/download/VERSION2.1/heideltime-standalone-2.1.zip'
+
+_heideltime_dir = _download_zip(url=_zip, name='Heideltime',
                                 check_dir='heideltime-standalone')
-_jar = join(_heideltime_dir, 'de.unihd.dbs.heideltime.standalone.jar')
 
+# In config.props.xtas, set the path to the TreeTagger home dir.
+_config_in = join(_heideltime_dir, 'config.props')
 _config_props = join(_heideltime_dir, 'config.props.xtas')
+with open(join(_heideltime_dir, 'config.props')) as config_in:
+    with open(_config_props, 'w') as config_out:
+        for line in config_in:
+            if line.startswith('treeTaggerHome = SET ME'):
+                treetagger_home = join(_make_data_home(), 'treetagger')
+                line = 'treeTaggerHome = %s\n' % treetagger_home
+            config_out.write(line)
 
-shutil.copyfile(join(_heideltime_dir, 'config.props'), _config_props)
-
-# Ugliest hack evah
-subprocess.call(['sed', '-i',
-                 's|^treeTaggerHome .*|treeTaggerHome = %s|'
-                    % (join(_make_data_home(), 'treetagger')),
-                 _config_props])
+_jar = join(_heideltime_dir, 'de.unihd.dbs.heideltime.standalone.jar')
 
 
 def call_heideltime(doc, language):
