e56f7e8722462bdcda434ff0b46632ad115639c3
Lars Buitinck <l.buitinck@esciencecenter.nl>
2016-03-29 14:06:17 +0200

    "coding: utf-8" must be on first line

--- xtas/tests/test_stanford.py
+++ xtas/tests/test_stanford.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # Copyright 2013-2015 Netherlands eScience Center and University of Amsterdam
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# coding: utf-8
 
 from nose.tools import assert_in, assert_true
 
