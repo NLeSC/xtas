916c257b31497d332293e2a2b7d88d2ce4c1e1ab
Lars Buitinck <l.buitinck@esciencecenter.nl>
2016-03-31 14:30:52 +0200

    refactor and test Heideltime initialization

--- xtas/tasks/_heideltime.py
+++ xtas/tasks/_heideltime.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import errno
+import os
 from os.path import join
+import shutil
 import subprocess
 import tempfile
 import xml.etree.ElementTree as etree
@@ -20,23 +23,59 @@ import xml.etree.ElementTree as etree
 from .._downloader import _download_zip, _make_data_home
 
 
-_zip = 'https://github.com/HeidelTime/heideltime/releases/download/VERSION2.1/heideltime-standalone-2.1.zip'
+def _set_treetagger_home(heideltime_dir):
+    """Point Heideltime to TreeTagger installation dir.
 
-_heideltime_dir = _download_zip(url=_zip, name='Heideltime',
-                                check_dir='heideltime-standalone')
+    Heideltime needs to know where TreeTagger lives (which we don't install
+    automatically for licensing reasons). We therefore edit its configuration
+    file, {heideltime_dir}/config.props, producing a new one at
+    {heideltime_dir}/config.props.xtas.
 
-# In config.props.xtas, set the path to the TreeTagger home dir.
-_config_in = join(_heideltime_dir, 'config.props')
-_config_props = join(_heideltime_dir, 'config.props.xtas')
-with open(join(_heideltime_dir, 'config.props')) as config_in:
-    with open(_config_props, 'w') as config_out:
-        for line in config_in:
-            if line.startswith('treeTaggerHome = SET ME'):
-                treetagger_home = join(_make_data_home(), 'treetagger')
-                line = 'treeTaggerHome = %s\n' % treetagger_home
-            config_out.write(line)
+    Returns the path of the new configuration file.
+    """
+    final_path = join(heideltime_dir, 'config.props.xtas')
+    home = None
 
-_jar = join(_heideltime_dir, 'de.unihd.dbs.heideltime.standalone.jar')
+    try:
+        in_path = join(heideltime_dir, 'config.props')
+        with open(in_path) as config_in:
+            config_out = tempfile.NamedTemporaryFile(dir=heideltime_dir,
+                                                     delete=False)
+            for line in config_in:
+                if line.startswith('treeTaggerHome = SET ME'):
+                    home = join(_make_data_home(), 'treetagger')
+                    line = 'treeTaggerHome = %s\n' % home
+                config_out.write(line)
+
+        if home is None:
+            raise Exception("didn't find treeTaggerHome in %r" % in_path)
+
+        shutil.move(config_out.name, final_path)
+
+    except:
+        os.remove(config_out.name)
+        raise
+
+    return final_path
+
+
+# None signals not initialized.
+_config_props = None
+_jar = None
+
+
+def _initialize():
+    # XXX We should synchronize across worker threads.
+    global _config_props, _jar
+    if _config_props is not None:
+        return
+
+    _zip = 'https://github.com/HeidelTime/heideltime/releases/download/VERSION2.1/heideltime-standalone-2.1.zip'
+
+    _heideltime_dir = _download_zip(url=_zip, name='Heideltime',
+                                    check_dir='heideltime-standalone')
+    _config_props = _set_treetagger_home(_heideltime_dir)
+    _jar = join(_heideltime_dir, 'de.unihd.dbs.heideltime.standalone.jar')
 
 
 def call_heideltime(doc, language, output):
--- xtas/tests/test_heideltime.py
+++ xtas/tests/test_heideltime.py
@@ -1,9 +1,37 @@
+import os
+from os import path
+from shutil import copyfile, rmtree
+from tempfile import mkdtemp
 import xml.etree.ElementTree as etree
 
 from nose import SkipTest
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_false, assert_raises
 
 from xtas.tasks import heideltime
+from xtas.tasks._heideltime import _set_treetagger_home
+
+
+def test_set_treetagger_home():
+    config_props = path.join(path.dirname(__file__), 'config.props')
+    directory = mkdtemp(prefix='xtas-heideltime-test.')
+    try:
+        temp = path.join(directory, 'config.props')
+        copyfile(config_props, path.join(directory, 'config.props'))
+        config_props_out = _set_treetagger_home(directory)
+
+        assert_equal(config_props_out,
+                     path.join(directory, 'config.props.xtas'))
+
+        # Test exception handling on empty config.props.
+        with open(temp, 'w'):
+            pass    # Empty file.
+        os.remove(config_props_out)
+
+        assert_raises(Exception, _set_treetagger_home, directory)
+        assert_false(path.exists(config_props_out))
+    finally:
+        rmtree(directory)
+        pass
 
 
 def test_heideltime():
