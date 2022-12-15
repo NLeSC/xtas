73cd8f387fa162ab5a79c7385138ca04e6cb0542
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-15 20:08:16 +0000

    Adding tarfile member sanitization to extractall()

--- xtas/tasks/_polarity.py
+++ xtas/tasks/_polarity.py
@@ -49,7 +49,26 @@ def _download():
             print("Downloading %r" % _TRAINING_DATA)
             urlretrieve(_TRAINING_DATA, temp.name, reporthook=progress)
             with tarfile.open(temp.name) as tar:
-                tar.extractall(path=data_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=data_dir)
 
     return training_dir
 
